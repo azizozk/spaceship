063b96c0b9367324f63677928a3dad8070103eba
aziz.ozkurt <azizozkurt@gmail.com>
2026-04-01 01:40:38 +0300

    PuduAccountLogCrudController add filters

--- app/src/Controller/Admin/PuduAccountLogCrudController.php
+++ app/src/Controller/Admin/PuduAccountLogCrudController.php
@@ -3,16 +3,31 @@
 namespace App\Controller\Admin;
 
 use App\Entity\PuduAccountLog;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\DateTimeFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\NumericFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
 
 class PuduAccountLogCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return PuduAccountLog::class;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            // ->add(EntityFilter::new('puduAccount'))
+            ->add(TextFilter::new('method'))
+            ->add(TextFilter::new('uri'))
+            ->add(NumericFilter::new('responseCode'))
+            ->add(DateTimeFilter::new('executedAt'));
     }
 
     /*
