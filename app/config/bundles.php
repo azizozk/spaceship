4fe140037b75de6ecd938b56e4b938a3720728b2
aziz.ozkurt <azizozkurt@gmail.com>
2026-04-01 01:32:53 +0300

    install doctrine fixtures bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
